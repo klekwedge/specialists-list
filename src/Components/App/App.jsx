48b52a1f7e48dcbff1a652e7731b6a431f9cffb4
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-31 16:45:57 +0300

    feat: add basic routing

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+
+const MainPage = lazy(() => import("../../pages/MainPage"));
+const DetailsPage = lazy(() => import("../../pages/DetailsPage"));
+const Page404 = lazy(() => import("../../pages/Page404"));
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={<h1>loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/:specialistId" element={<DetailsPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
