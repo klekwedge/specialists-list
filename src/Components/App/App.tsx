36d709d8ccf7afa1a20a67fa74511becfb3f753a
Andrey <antonov.andrey.10101@gmail.com>
2023-05-08 18:14:42 +0300

    feat: add Loader component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loader from "../Loader/Loader";
 
 const MainPage = lazy(() => import("../../pages/MainPage"));
 const DetailsPage = lazy(() => import("../../pages/DetailsPage"));
@@ -7,9 +8,9 @@ const Page404 = lazy(() => import("../../pages/Page404/Page404"));
 
 function App() {
   return (
-    <div className='_container'>
+    <div className="_container">
       <Router>
-        <Suspense fallback={<h1>loading...</h1>}>
+        <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/:specialistId" element={<DetailsPage />} />
---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import "./Loader.scss";
+
+function Loader() {
+  return (
+    <div className="container">
+	<div className="blue"></div>
+	<div className="blue"></div>
+	<div className="blue"></div>
+	<div className="blue"></div>
+</div>
+  );
+}
+
+export default Loader;
